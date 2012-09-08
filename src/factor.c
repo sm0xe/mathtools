a9d1c440f219d119c516702f3e40e4f5fb7db676
Karl August Hoivik <gussefant@gmail.com>
2012-09-08 22:49:34 +0200

    Global array

--- src/factor.c
+++ src/factor.c
@@ -4,13 +4,12 @@
 #ifdef HAVE_PTHREAD_H
 #include <pthread.h>
 #endif
+int primes[100000];
 void* prime_find(void* arg){
 	long int num = *((int*) arg); //The number we have to divide.
 	int prime; //The prime we will eventually find.
 	/*A shitload of primes to check.*/
-	int primes[num];
 	int primcount=0;
-	prime_sieve(num, 0, primes);
 	while(primes[primcount] != 0){
 		if((num % primes[primcount])==0) //Try to divide the number by the largest prime.
 		{
@@ -24,10 +23,10 @@ void* prime_find(void* arg){
 void addSpaces(int num){
 	int number = num;
 	short digits = 0; //A variable to hold the number of digits.
-	char spaces[5] = "     "; //A character string containing 5 spaces.
+	char spaces[5] = "     ";
 	while(number>=10){
-		++digits; //Increment digits.
-		number/=10;  //And divide the number by 10.
+		++digits;
+		number/=10;
 	}
 	spaces[5-digits] = '\0'; //Terminate the string.
 	printf("%s", spaces);
@@ -35,14 +34,15 @@ void addSpaces(int num){
 void factor(long int num){
 	long int number = num; //The number to be factored.
 	pthread_t thread; //The thread we use to find the prime.
+	prime_sieve(num, 0, primes);
 	int prime;
 	while(number>1){
 		pthread_create(&thread, NULL, &prime_find, &number); //Set the thread to work.
 		addSpaces(number); //Align the numbers.
-		printf("%d|", number); //And print the number.
+		printf("%d|", number);
 		meaningOfLife(); //Think about the meaning of life while we wait for the thread to finish.
 		pthread_join(thread, (void*) &prime); //get the prime from the thread.
-		printf("%d\n", prime); //Print the prime.
+		printf("%d\n", prime);
 		number/=prime;
 	}
 	addSpaces(number);
