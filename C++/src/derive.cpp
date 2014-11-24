13117075f3f383048363e8ddc3bf3762d56f1217
Karl August Hoivik <gussefant@gmail.com>
2014-11-24 22:12:04 +0100

    Improved parsing and added the actual derivation functionality

--- C++/src/derive.cpp
+++ C++/src/derive.cpp
@@ -5,54 +5,71 @@
 #include <vector>
 using namespace std;
 typedef struct{
-	int factor;
+	int coef;
 	char var;
 	int exp;
-}elements;
-elements extract(string element){
-	elements parts;
+}term_s;
+term_s deconstruct_term(string element){
+	term_s term;
 	int pos=0;
 	pos = element.find_first_of("xyzt");
-	stringstream(element.substr(0,pos)) >> parts.factor;
-	if(pos!=string::npos){
-		parts.var = element[pos];
+	if(element[pos-1]!='-' && element[pos-1]!='+'){
+		stringstream(element.substr(0,pos)) >> term.coef;
 	}
 	else{
-		parts.exp=0;
-		parts.var='-';
-		return parts;
+		term.coef=1;
+	}
+	if(pos!=string::npos){
+		term.var = element[pos];
+	}
+	else{
+		term.exp=0;
+		term.var='-';
+		return term;
 	}
 	if((pos+1)!=element.length()){
-		stringstream(element.substr(pos+2)) >> parts.exp;
+		stringstream(element.substr(pos+2)) >> term.exp;
 	}
 	else{
-		parts.exp=1;
+		term.exp=1;
 	}
-	return parts;
+	return term;
+}
+string construct_term(term_s term){
+	stringstream construct;
+	if(term.coef>0 && term.coef!=0 )
+		construct << '+';
+	if((term.coef!=1 && term.coef!=0) || (term.coef==1 && term.exp==0))
+		construct << term.coef;
+	if(term.var!='-' && term.coef!=0 && term.exp!=0)
+		construct << term.var;
+	if(term.exp!=-1 && term.exp!=0 && term.exp!=1)
+		construct << '^' << term.exp;
+	return construct.str();
 }
 vector<string> parse_poly(string poly){
 	vector<string> elements;
 	size_t pos = poly.find_first_of("+-",1);
-	do{
+	while(pos!=string::npos){
 		string element = poly.substr(0,pos);
 		elements.push_back(element);
 		poly = poly.substr(pos);
 		pos = poly.find_first_of("+-",1);
-	}while(pos!=string::npos);
-	string element = poly.substr(0,pos);
+	};
+	string element = poly;
 	elements.push_back(element);
 	return elements;
 }
-int derive(string element){
-	vector<string> polynomial = parse_poly(element);
-	for(int i=0; i<polynomial.size(); i++){
-		elements poly = extract(polynomial.at(i));
-		cout << poly.factor << endl;
-		cout << poly.var << endl;
-		cout << poly.exp << endl;
-		cout << "---" << endl;
+void derive(string element){
+	vector<string> poly = parse_poly(element);
+	string derived;
+	for(int i=0;i<(poly.size());i++){
+		term_s term = deconstruct_term(poly.at(i));
+		term.coef*=term.exp;
+		term.exp-=1;
+		derived+=construct_term(term);
 	}
-	return 0;
+	cout << derived << endl;
 }
 int main(){
 	string element;
