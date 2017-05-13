16f127197ad112af646e63f24fa1614992ad9cce
Karl August Hoivik <gussefant@gmail.com>
2017-05-13 12:42:11 +0200

    Switched to more efficient implementation of prime sieve

--- C++/src/primes.cpp
+++ C++/src/primes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <signal.h>
+#include <math.h>
 using namespace std;
 int primecount;
 extern int lastprime;
@@ -14,49 +15,39 @@ void prime_sieve(long long int limit){
 		cout << "A prime number must be more than 1." << endl;
 		exit(0);
 	}
-	vector<int> temp;
-	temp.push_back(2);
-	cout << "2 ";
-	int i=3;
 	primecount=1;
-	do{
-		short int isPrime = 1;
-		for(int j=0; j<temp.size(); j++){
-			if((i % temp.at(j)) == 0){
-				isPrime = 0;
-				break;
-			}
+	bool isprime[limit];
+	for(int i=0; i<limit; i++){
+	    isprime[i]=true;
+	}
+	for(int i=2; i<limit; i++){
+	    if(isprime[i]==true){
+		primecount++;
+		cout << i << " ";
+		for(int j=i*i; j<limit; j+=i){
+		    isprime[j]=false;
 		}
-		if(isPrime){
-			temp.push_back(i);
-			primecount++;
-			cout << i << " ";
-		}
-		i+=2;
-	}while(i<=limit);
+	    }
+	}
 	cout << endl;
 }
 void prime_sieve(vector<long long int>* prime_array){
 	quit_siever = 0;
-	(*prime_array).push_back(2);
-	int i=3;
-	primecount=1;	
-	short int isPrime = 1;
-	do{
-		short int isPrime = 1;
-		for(int j=0; j<(*prime_array).size(); j++){
-			if((i % (*prime_array).at(j)) == 0){
-				isPrime = 0;
-				break;
-			}
-			else if(quit_siever == 1) break;
+	primecount=0;
+	bool isprime[*limit];
+	for(int i=0; i<*limit; i++){
+	    isprime[i]=true;
+	}
+	for(int i=2; i<*limit; i++){
+	    if(isprime[i]==true){
+	    	(*prime_array).push_back(i);
+		primecount++;
+		if(stats) lastprime=i;
+		for(int j=i*i; j<*limit; j+=i){
+		    isprime[j]=false;
 		}
-		if(isPrime){
-			(*prime_array).push_back(i);
-			primecount++;
-			if(stats) lastprime = i;
-		}
-		i+=2;
-	}while(i<=*limit && (quit_siever == 0));
+	    }
+	    if(quit_siever==1) break;
+	}
 	if(quit_siever==0) prime_finished=1;
 }
